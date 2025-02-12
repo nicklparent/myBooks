aecfec8c3df1571a6d35f3986d4a64d54a34a2ff
Nicholas Parent <nick.lparent@outlook.com>
2025-02-12 01:39:11 -0400

    front end foundations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import Header from './assets/sections/Header';
 
 
 export function App(){
   return(
-    <div>
-      Hello World
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
