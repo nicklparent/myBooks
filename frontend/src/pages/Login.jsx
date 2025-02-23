05d1c4c91f222275d5d5618d4c51ff3acb49a29a
Nicholas Parent <nick.lparent@outlook.com>
2025-02-23 00:25:16 -0400

    updates

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,17 @@
-/**
- * UI will be a book where the first page login options and the second page is createaccount options
- * 
- */
-
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../assets/css/login.css';
 
-
-export function Login(){
-    
-    
+export function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [userName, setUserName] = useState("");
     const [loggedIn, setLoggedIn] = useState(false);
-    
+    const [loginType, setLoginType] = useState("login");
+
     const createAccountHandler = async () => {
-        const response = fetch('http://localhost:5053/user', {
+        const response = await fetch('http://localhost:5053/user', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
@@ -33,82 +26,126 @@ export function Login(){
         });
 
         const data = await response.json();
-        if (data.message == 'Account created Succsesfully'){
+        if (data.message === 'Account created Successfully') {
             setLoggedIn(true);
         }
     }
 
-    useEffect(() => {
+    const loginHandler = () => {
+        // Implement your login logic here
+    }
 
-    }, []);
+    const toggleLoginType = () => {
+        setLoginType(prevType => prevType === "login" ? "register" : "login");
+    }
 
-    if (loggedIn){
+    if (loggedIn) {
         window.location.href = 'http:localhost:3000/';
     }
 
     return (
         <div className='center screen'>
-            <div className='signin-page'>
-                {/* Login Side */}
-                <div className='login'>
-                    <h2>Welcome Back!</h2>
-                    <h3>Login</h3>
-                    <div className='input-field'>
-                        <label htmlFor="email-login">Email</label>
-                        <input 
-                            type='email'
-                            name='email-login'
-                            id='email-login' 
-                            className='input-in' 
-                            placeholder='example@mybooks.com'
-                            onChange={e => setEmail(e.target.value)}    
-                        />
-                    </div>
+            <div className='signin-page' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                
 
-                    <div className='input-field'>
-                        <label htmlFor="password-login">Password</label>
-                        <input 
-                            type='password'
-                            name='password-login'
-                            id='password-login' 
-                            className='input-in' 
-                            placeholder='Enter Password'
-                            onChange={e => setPassword(e.target.value)}    
-                        />
-                    </div>
-                </div>
-                <div className='middle-line'>
+                {loginType === "login" ? (
+                    // Login Side
+                    <div className='login'>
+                        <h2>Welcome Back!</h2>
+                        <h3>Login</h3>
+                        <div className='input-field'>
+                            <label htmlFor="email-login">Email</label>
+                            <input
+                                type='email'
+                                name='email-login'
+                                id='email-login'
+                                className='input-in'
+                                placeholder='example@mybooks.com'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </div>
 
-                </div>
-                {/* Create account side */}
-                <div className='register'>
-                    <h2>New Reader?</h2>
-                    <h3>Register</h3>
-                    <div className='input-field'>
-                        <label htmlFor="email-login-register">Email</label>
-                        <input 
-                            type='email'
-                            name='email-login'
-                            id='email-login-register' 
-                            className='input-in' 
-                            placeholder='example@mybooks.com'
-                            onChange={e => setEmail(e.target.value)}    
-                        />
+                        <div className='input-field'>
+                            <label htmlFor="password-login">Password</label>
+                            <input
+                                type='password'
+                                name='password-login'
+                                id='password-login'
+                                className='input-in'
+                                placeholder='Enter Password'
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <button onClick={loginHandler} className='sign-in btn'>Sign In</button>
                     </div>
-                    <div className='input-field'>
-                        <label htmlFor="password-login-register">Password</label>
-                        <input 
-                            type='password'
-                            name='password-login-register'
-                            id='password-login-register' 
-                            className='input-in' 
-                            placeholder='Enter Password'
-                            onChange={e => setPassword(e.target.value)}    
-                        />
+                ) : (
+                    // Create account side
+                    <div className='register'>
+                        <h2>New Reader?</h2>
+                        <h3>Register</h3>
+                        <div className='input-field'>
+                            <label htmlFor="email-login-register">Email</label>
+                            <input
+                                type='email'
+                                name='email-login'
+                                id='email-login-register'
+                                className='input-in'
+                                placeholder='example@mybooks.com'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className='input-field'>
+                            <label htmlFor="password-login-register">Password</label>
+                            <input
+                                type='password'
+                                name='password-login-register'
+                                id='password-login-register'
+                                className='input-in'
+                                placeholder='Enter Password'
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className='input-field'>
+                            <label htmlFor="first-name">First Name</label>
+                            <input
+                                type='text'
+                                name='first-name'
+                                id='first-name'
+                                className='input-in'
+                                placeholder='First Name'
+                                onChange={e => setFirstName(e.target.value)}
+                            />
+                        </div>
+                        <div className='input-field'>
+                            <label htmlFor="last-name">Last Name</label>
+                            <input
+                                type='text'
+                                name='last-name'
+                                id='last-name'
+                                className='input-in'
+                                placeholder='Last Name'
+                                onChange={e => setLastName(e.target.value)}
+                            />
+                        </div>
+                        <div className='input-field'>
+                            <label htmlFor="user-name">Username</label>
+                            <input
+                                type='text'
+                                name='user-name'
+                                id='user-name'
+                                className='input-in'
+                                placeholder='Username'
+                                onChange={e => setUserName(e.target.value)}
+                            />
+                        </div>
+                        <button onClick={createAccountHandler} className='sign-in btn'>Create Account</button>
                     </div>
-                </div>
+                )}
+                <button onClick={toggleLoginType} className='toggle-btn'>
+                    {loginType === "login" ? "New reader? Create an account" : "Already have an account?"}
+                </button>
             </div>
         </div>
     );
 }
-
