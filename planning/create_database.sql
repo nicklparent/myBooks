f371fe643009a6c5bc8e110673409bf116ca4a61
Nicholas Parent <nick.lparent@outlook.com>
2025-02-25 16:01:20 -0400

    documentation

--- planning/create_database.sql
+++ planning/create_database.sql
@@ -2,17 +2,18 @@ CREATE DATABASE IF NOT EXISTS my_books;
 USE my_books;
 
 CREATE TABLE `my_books`.`books` (
-	`id` INT NOT NULL,
-	`Title` VARCHAR(45) NULL,
+	`Id` INT UNIQUE NOT NULL,
+	`Title` VARCHAR(45) NOT NULL,
   `Author` VARCHAR(45) NULL,
   `Genre` VARCHAR(45) NULL,
-PRIMARY KEY (`id`));
+  `PageCount` INT NULL,
+PRIMARY KEY (`Id`));
 
 CREATE TABLE `my_books`.`users` (
-  `Id` INT NOT NULL,
+  `Id` INT UNIQUE NOT NULL,
+  `Username` VARCHAR(100) UNIQUE NOT NULL,
   `Email` VARCHAR(100) NOT NULL,
   `FirstName` VARCHAR(45) NULL,
   `LastName` VARCHAR(45) NULL,
   `Password` VARCHAR(100) NOT NULL,
-  `Username` VARCHAR(100) NOT NULL,
 PRIMARY KEY (`Id`));
