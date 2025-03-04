d484bf48be7ac15b429c2f83e361141bb5bfb0fa
Nicholas Parent <nick.lparent@outlook.com>
2025-03-04 00:40:51 -0400

    flutter controller started

--- mobile/lib/pages/HomePage.dart
+++ mobile/lib/pages/HomePage.dart
@@ -1,8 +1,35 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:mobile/API/BookController.dart';
 import 'package:mobile/sections/Footer.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage>{
+  List<dynamic> books = [];
+
+  Future<void> updateBooks() async{
+    try {
+      BookController bookController = new BookController();
+      var res = await bookController.getAll();
+
+      books[0] = res.body;
+    } catch (e) {
+      books = [];
+    }
+  }
+  @override
+  void initState() {
+    updateBooks();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +37,16 @@ class HomePage extends StatelessWidget{
     return(
         Scaffold(
           body: Column(
+            children: [
+              ListView.builder(
+                  itemCount: books.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
 
+                    );
+                  }
+              )
+            ],
           ),
           bottomNavigationBar: Footer(),
         )
