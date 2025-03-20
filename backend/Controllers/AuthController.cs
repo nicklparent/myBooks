3c4ba2b2908169068eb8d4a7dde5bdbe839dbc43
nparent <nc838713@dal.ca>
2025-03-19 21:30:54 -0300

    changes

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -2,8 +2,11 @@
 using backend.Models;
 using backend.services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MySql.Data.MySqlClient;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 namespace backend.Controllers
 {
     [ApiController]
@@ -32,6 +35,25 @@ namespace backend.Controllers
 
             if (user != null) {
                 var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenKey = Encoding.UTF8.GetBytes(key);
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email.ToString()),
+                    new Claim(ClaimTypes.Name, user.FirstName.ToString() ?? "")
+                };
+
+                var tokenDescription = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(30),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(tokenKey),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+
+
             }
 
             return StatusCode(500, "Something went wrong");
